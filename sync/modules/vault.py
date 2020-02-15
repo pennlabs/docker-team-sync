74b46e04170f5f8d452b8bc90016ca48fa30edd8
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-02-15 12:54:27 -0500

    Admin policy

--- sync/modules/vault.py
+++ sync/modules/vault.py
@@ -15,6 +15,7 @@ def sync(teams):
         return
 
     if not client.sys.is_sealed():
+        # Apply normal team policies
         with open("sync/modules/user-policy.hcl.j2") as f:
             t = Template(f.read())
             for team in teams["leads"]:
@@ -23,5 +24,11 @@ def sync(teams):
                 pol = t.render(team_name=base_team_slug, repos=repos)
                 client.sys.create_or_update_policy(name=base_team_slug, policy=pol)
                 client.auth.github.map_team(team_name=team.slug, policies=[base_team_slug])
+        # Apply admin policy
+        admin_team_name = "sre"
+        with open("sync/modules/admin-policy.hcl") as admin_policy:
+            client.sys.create_or_update_policy(name=admin_team_name, policy=admin_policy.read())
+            client.auth.github.map_team(team_name=admin_team_name, policies=[admin_team_name])
+
     else:
         print("vault: Vault sealed. Stopping.")
